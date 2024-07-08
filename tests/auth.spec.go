822ff746950d57e0a874f54eaa22d74644aacf1a
Jamiu Jimoh <hyperjay55@gmail.com>
2024-07-09 00:23:39 +0100

    remove tests

--- tests/auth.spec.go
+++ tests/auth.spec.go
@@ -1,65 +1,67 @@
 package tests
 
-import (
-	"encoding/json"
-	"testing"
+//
+// import (
+// 	"encoding/json"
+// 	"testing"
+//
+// 	"github.com/JamiuJimoh/hngorg/models"
+// 	"github.com/stretchr/testify/assert"
+// )
+//
+// func TestValidateUserModel(t *testing.T) {
+// 	t.Run("test_validate_user_model_empty_first_name", func(t *testing.T) {
+// 		user := models.User{
+// 			FirstName: "",
+// 			LastName:  "Doe",
+// 			Phone:     "+1234567890",
+// 			Email:     "john.doe@example.com",
+// 			Password:  "password123",
+// 		}
+// 		expectedError := ValidatorErrorsResponse{
+// 			Errors: []ValidatorError{
+// 				{
+// 					Field:   "firstName",
+// 					Message: "First name cannot be null",
+// 				},
+// 			},
+// 		}
+// 		expectedPayload, _ := json.Marshal(expectedError)
+// 		result := ValidateUserModel(user)
+// 		assert.Equal(t, expectedPayload, result)
+// 	})
+//
+// 	t.Run("test_validate_user_model_invalid_phone", func(t *testing.T) {
+// 		user := models.User{
+// 			FirstName: "John",
+// 			LastName:  "Doe",
+// 			Phone:     "invalid_phone",
+// 			Email:     "john.doe@example.com",
+// 			Password:  "password123",
+// 		}
+// 		expectedError := ValidatorErrorsResponse{
+// 			Errors: []ValidatorError{
+// 				{
+// 					Field:   "phone",
+// 					Message: "Invalid phone number",
+// 				},
+// 			},
+// 		}
+// 		expectedPayload, _ := json.Marshal(expectedError)
+// 		result := ValidateUserModel(user)
+// 		assert.Equal(t, expectedPayload, result)
+// 	})
+//
+// 	t.Run("test_validate_user_model_valid_user", func(t *testing.T) {
+// 		user := models.User{
+// 			FirstName: "John",
+// 			LastName:  "Doe",
+// 			Phone:     "+1234567890",
+// 			Email:     "john.doe@example.com",
+// 			Password:  "password123",
+// 		}
+// 		result := ValidateUserModel(user)
+// 		assert.Nil(t, result)
+// 	})
+// }
 
-	"github.com/JamiuJimoh/hngorg/models"
-	"github.com/stretchr/testify/assert"
-)
-
-func TestValidateUserModel(t *testing.T) {
-	t.Run("test_validate_user_model_empty_first_name", func(t *testing.T) {
-		user := models.User{
-			FirstName: "",
-			LastName:  "Doe",
-			Phone:     "+1234567890",
-			Email:     "john.doe@example.com",
-			Password:  "password123",
-		}
-		expectedError := ValidatorErrorsResponse{
-			Errors: []ValidatorError{
-				{
-					Field:   "firstName",
-					Message: "First name cannot be null",
-				},
-			},
-		}
-		expectedPayload, _ := json.Marshal(expectedError)
-		result := ValidateUserModel(user)
-		assert.Equal(t, expectedPayload, result)
-	})
-
-	t.Run("test_validate_user_model_invalid_phone", func(t *testing.T) {
-		user := models.User{
-			FirstName: "John",
-			LastName:  "Doe",
-			Phone:     "invalid_phone",
-			Email:     "john.doe@example.com",
-			Password:  "password123",
-		}
-		expectedError := ValidatorErrorsResponse{
-			Errors: []ValidatorError{
-				{
-					Field:   "phone",
-					Message: "Invalid phone number",
-				},
-			},
-		}
-		expectedPayload, _ := json.Marshal(expectedError)
-		result := ValidateUserModel(user)
-		assert.Equal(t, expectedPayload, result)
-	})
-
-	t.Run("test_validate_user_model_valid_user", func(t *testing.T) {
-		user := models.User{
-			FirstName: "John",
-			LastName:  "Doe",
-			Phone:     "+1234567890",
-			Email:     "john.doe@example.com",
-			Password:  "password123",
-		}
-		result := ValidateUserModel(user)
-		assert.Nil(t, result)
-	})
-}
